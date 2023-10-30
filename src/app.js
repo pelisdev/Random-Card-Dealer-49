e4787b1103955f2004fda7a3deb6fff0bda457ac
Alvaro <curielyseguro@gmail.com>
2023-10-30 16:08:17 +0000

    listo

--- src/app.js
+++ src/app.js
@@ -6,6 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(randomSuit());
+  document.querySelector(".number").innerHTML = randomPalos();
+};
+
+let randomPalos = () => {
+  let palos = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let Palos = Math.floor(Math.random() * palos.length);
+  return palos[Palos];
+};
+
+let randomSuit = () => {
+  let suit = ["diamond", "spade", "heart", "club"];
+  let iSuit = Math.floor(Math.random() * suit.length);
+  return suit[iSuit];
 };
